7d7177a088e58c2fdef0916fbddadec9beb0b1a5
ShishirSaja <shishirsaja17@gmail.com>
2025-07-23 23:42:52 +0530

    update bitM

--- bitManipulation/oddEven.java
+++ bitManipulation/oddEven.java
@@ -1,6 +1,15 @@
 import java.util.Scanner;
 
 public class oddEven {
+
+    public static void power(int var0) {
+      if ((var0 & var0 - 1) == 0) {
+         System.out.println("the number is power of 2");
+      } else {
+         System.out.println("the number is not power of 2");
+      }
+
+   }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.println("give the number :");
