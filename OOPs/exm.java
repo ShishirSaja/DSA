ce1086e22fbbd13205a2499fa52cc8b79f17c2c6
ShishirSaja <shishirsaja17@gmail.com>
2025-08-01 19:56:22 +0530

    update oops

--- OOPs/exm.java
+++ OOPs/exm.java
@@ -1,16 +1,30 @@
 
 public class exm {
+    void eat(){
+        System.out.println("eating  in the main something");
+
+    }
+
 
     public static void main(String args[]){
+
+        // exm et = new exm();
+        // et.eat();
+
         dog puppy = new dog();
+        // puppy.eat();
+        // puppy.mouth = "one";
+
+        // pummorian pm = new pummorian();
+        // pm.eat();
+
+        // fish fs = new fish();
+        // fs.eat();
+
+        Animal an = new Animal();
+        an.eat("bone");
+        an.eat();
         puppy.eat();
-        puppy.mouth = "one";
-
-        pummorian pm = new pummorian();
-        pm.eat();
-
-        fish fs = new fish();
-        fs.eat();
 
     }
     
@@ -23,12 +37,24 @@ class Animal{
         System.out.println("eating something");
 
     }
+
+    String eat(String food){
+        return food;
+    }
+
+    
 }
 
 class dog extends Animal{
     void bark(){
         System.out.println("dogs barks");
     }
+
+    void eat(){
+        System.out.println("dog eating something");
+
+    }
+
 }
 
 class fish extends Animal{
@@ -40,5 +66,15 @@ class fish extends Animal{
 class pummorian extends dog {
     void height(){
         System.out.println("short");
+    }
+
+    //code from another file for git only 
+    public void  Pen(){
+        System.out.println("creating new object of class pen");
+    }
+
+    public void Pen(Pen pn){
+        this.color = pn.color; 
+
     }
 }
